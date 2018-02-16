1703cdf48b763a23248c75e4dd12167c72e8f9b9
Nadav Appel <nadavp3@gmail.com>
2018-02-15 22:56:42 -0800

    Add temp runner code at end of file

--- BigFiveResultsTextSerializer.rb
+++ BigFiveResultsTextSerializer.rb
@@ -29,3 +29,7 @@ class BigFiveResultsPoster
   end
 
 end
+
+text_read = File.read("full_results.txt")
+flow_start = BigFiveResultsTextSerializer.new(text_read)
+last_phase = BigFiveResultsPoster.new(flow_start, "user@example.com")
